7a6922a81f69e8dff65359479d1454967e1c03a0
chanceyliu <1445885764@qq.com>
2023-11-21 17:26:50 +0800

    chore: 替换eslint配置

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,7 +1,18 @@
-import { FC } from 'react'
+import {
+  FC,
+  // useEffect, useState
+} from 'react'
 import styles from './app.module.less'
 
 const App: FC = () => {
+  // const [str, setStr] = useState(false)
+
+  // useEffect(() => {
+  //   if (str) {
+  //     console.log('999')
+  //   }
+  // }, [])
+
   return (
     <div className={styles.page}>
       <h1>基础模板</h1>
